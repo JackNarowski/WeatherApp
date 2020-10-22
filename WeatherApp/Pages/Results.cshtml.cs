18421a4a51b2e51d8ae09f8cabe687a89f225a00
JackNarowski <jacknarowski@gmail.com>
2020-10-22 01:02:05 -0500

    Added Results page

---
+++ WeatherApp/Pages/Results.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherApp
+{
+    public class ResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
